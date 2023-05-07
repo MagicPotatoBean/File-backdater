d0716353ef71492e57b75c86f65373c8d07f9f58
MagicPotatoBean <leo@soutter.com>
2023-05-07 15:42:38 +0100

    Initial commit

---
+++ File backdater/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = "dd/MM/yyyy HH:mm:ss"
+        Me.DateTimePicker1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker1.Location = New System.Drawing.Point(0, 0)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(140, 20)
+        Me.DateTimePicker1.TabIndex = 0
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button1.Location = New System.Drawing.Point(0, 20)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(140, 24)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Select file"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(140, 44)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(156, 83)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(156, 83)
+        Me.Name = "Form1"
+        Me.Text = "Backdater"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button1 As Button
+End Class
---
+++ File backdater/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim filenames() As String
+        With OpenFileDialog1
+            .Title = "Select file to backdate"
+            .Multiselect = True
+            .ShowDialog()
+            filenames = .FileNames
+        End With
+        If filenames.Length > 0 Then
+            For Each filename In filenames
+                Try
+                    If File.Exists(filename) Then
+                        File.SetCreationTime(filename, DateTimePicker1.Value)
+                    End If
+                Catch ex As Exception
+                    MsgBox(ex)
+                End Try
+            Next
+            MsgBox("Done!")
+        Else
+            MsgBox("No files selected")
+        End If
+    End Sub
+End Class
